8efff65fa220698e005cb7e9e158a1147fa1d2e2
CatsAreJumpers <104035131+CatsAreJumpers@users.noreply.github.com>
2023-06-08 22:17:22 -0400

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -11,6 +11,7 @@ const serve = new nodeStatic.Server('public/');
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const certDir = join(__dirname, 'ssl');
 const options = {
     key: fs.readFileSync(certDir, 'key.pem'),
     cert: fs.readFileSync(certDir, 'cert.pem')
