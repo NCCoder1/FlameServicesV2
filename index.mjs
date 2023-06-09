940da035ac175b6197591bf59216f6570d11b3a1
CatsAreJumpers <104035131+CatsAreJumpers@users.noreply.github.com>
2023-06-09 07:38:52 -0400

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,27 +1,13 @@
 import Server from './bare/Server.mjs';
 import { readFileSync } from 'fs';
-import https from 'https';
-import fs from 'fs';
+import http from 'http';
 import nodeStatic from 'node-static';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+
 
 const bare =  new Server('/bare/', '');
 const serve = new nodeStatic.Server('public/');
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const certDir = join(__dirname, 'ssl');
-const options = {
-  key: fs.readFileSync(join(certDir, 'key.pem'), 'utf8'),
-  cert: fs.readFileSync(join(certDir, 'cert.pem'), 'utf8')
-};
-
-
-const server = https.createServer(options);
-
-
-
+const server = http.createServer();
 
 server.on('request', (request, response) => {
     if (bare.route_request(request, response)) return true;
@@ -33,4 +19,4 @@ server.on('upgrade', (req, socket, head) => {
 	socket.end();
 });
 
-server.listen(process.env.PORT || 443);
+server.listen(process.env.PORT || 80);
